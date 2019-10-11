a98a98dbca33a39c4e494259dfa6cda5f72f31a9
Vipin Kumar V <vipink1203@gmail.com>
2019-10-11 17:19:59 +0530

    Update handler.py

--- iam_rotation/handler.py
+++ iam_rotation/handler.py
@@ -1,3 +1,9 @@
+#	Author:	@vipink1203
+# 	Description: API endpoint will take an IAM Access key as query parameter and will check for the user associated with that key. 
+#                If the key is valid it will create a new key pair and display it. 
+#                You need to have additional lambda functions for IAM key rotation.
+#	Last	Edited:	October 11 2019
+
 import json
 import boto3
 import datetime
